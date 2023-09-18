a297d8a3f33b1fa71f6d71672e8c29a30644424a
Friso van Beek <frisfro1998@gmail.com>
2023-09-19 01:59:10 +0200

    Working user input and playtime

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -4,11 +4,25 @@ import simpleaudio as sa
 
 # Sample location
 
-wave_obj = sa.WaveObject.from_wave_file("/home/friso-linux/Documents/HKU/Jaar_2/CSD2/Assets/kick.mp3")
+wave_obj = sa.WaveObject.from_wave_file("../../Assets/snare.wav")
 
-# Play sample
+# Define sample playing function
 
-wave_obj.play()
+def playSample(amount):
+    for x in range(amount):
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+
+# User interaction
+
+amountOfTimes = int(input("Please enter the amount of times you would like for the sample to be played: "))
+print(amountOfTimes, "times.")
+
+# Run sample playing function
+
+playSample(amountOfTimes)
+
+
 
 # import wave
 
