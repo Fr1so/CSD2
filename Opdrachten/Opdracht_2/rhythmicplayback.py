92afc4a43e36a03c0b733739371f729231616299
Friso van Beek <frisfro1998@gmail.com>
2023-09-26 09:36:34 +0200

    Start of rhythmic playback (Opdracht_2)

---
+++ Opdrachten/Opdracht_2/rhythmicplayback.py
@@ -0,0 +1,10 @@
+# Libraries
+
+import simpleaudio as sa
+import time
+
+# User Input
+
+numPlaybackTimes = int(input("Please enter the amount of times you would like for the sample to be played: "))
+
+print(numPlaybackTimes, "times.")
