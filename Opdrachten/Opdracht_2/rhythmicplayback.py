203c46d1da8356b2240a37b177bf46052fc6ba3d
Friso van Beek <frisfro1998@gmail.com>
2023-09-26 09:39:11 +0200

    Added bpm input

--- Opdrachten/Opdracht_2/rhythmicplayback.py
+++ Opdrachten/Opdracht_2/rhythmicplayback.py
@@ -1,10 +1,20 @@
-# Libraries
+## Libraries
 
 import simpleaudio as sa
 import time
 
-# User Input
+## User Input
 
-numPlaybackTimes = int(input("Please enter the amount of times you would like for the sample to be played: "))
+# Ask for amount of times to play back sound.
+
+numPlaybackTimes = int(input("Please enter the amount of times you would like for the sample to be played as an integer: "))
 
 print(numPlaybackTimes, "times.")
+
+# Ask for bpm
+
+bpm = float(input("Please enter the bpm as a float: "))
+quarterNote = (60.0 / bpm)
+
+print("Bpm: ", bpm, "Quarternote duration: ", quarterNote, "sec")
+
