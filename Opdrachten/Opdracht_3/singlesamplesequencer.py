45d3b5dda03d33118a91d623df4aa5b05861dc75
Friso van Beek <frisfro1998@gmail.com>
2023-09-26 09:54:47 +0200

    Reusing some code from other previous assignment

---
+++ Opdrachten/Opdracht_3/singlesamplesequencer.py
@@ -0,0 +1,14 @@
+## Libraries
+
+import simpleaudio as sa
+import time
+
+## User Input
+
+# Ask the user for amount of times to play back sound.
+
+numPlaybackTimes = int(input("Please enter the amount of times you would like for the sample to be played as an integer: "))
+
+print(numPlaybackTimes, "times.")
+
+# Ask the user for bpm
