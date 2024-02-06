41e460df8afe43a2e957205a17aac907e3b09953
Friso van Beek <frisfro1998@gmail.com>
2024-02-06 15:36:00 +0100

    Make class based on example on classboard

--- Blok_C/Opdrachten/Circular_Buffer.cpp
+++ Blok_C/Opdrachten/Circular_Buffer.cpp
@@ -1,2 +1,13 @@
 // Friso's Circular Buffer example
 
+class Circular_Buffer {
+    public:
+        float* buffer;
+        int readH, writeH;
+        void empty();
+        void write(float sample);
+        float read();
+    private: 
+        void writeH;
+        void readH;
+}
